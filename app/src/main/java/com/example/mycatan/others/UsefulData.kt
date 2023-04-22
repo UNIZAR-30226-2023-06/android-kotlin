4e689e2ae4e496128a767929233e787b0dcac6fc
JTorci <javier.torcida.delamo@gmail.com>
2023-04-22 18:39:49 +0200

    fotosfichas + vertices y aristas

--- app/src/main/java/com/example/mycatan/others/UsefulData.kt
+++ app/src/main/java/com/example/mycatan/others/UsefulData.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Vertices
 
 //Javi 192.168.1.39
 val ipBackend = "192.168.1.39"
@@ -32,5 +33,33 @@ object Partida {
     lateinit var Roca: String
     lateinit var Trigo: String
     lateinit var Ovejas: String
-
+    lateinit var Vertices: HashMap<String,String>
+    lateinit var Aristas: HashMap<String,String>
 }
+
+fun inicializarVertices() {
+    val claves = listOf(
+        "38", "5A", "7C", "27", "49", "6B", "8D", "36", "58", "7A", "9C", "25", "47", "69", "8B", "AD",
+        "34", "56", "78", "9A", "BC", "23", "45", "67", "89", "AB", "CD", "32", "54", "76", "98", "BA",
+        "DC", "43", "65", "87", "A9", "CB", "52", "74", "96", "B8", "DA", "63", "85", "A7", "C9", "72",
+        "94", "B6", "D8", "83", "A5", "C7"
+    )
+    for (clave in claves) {
+        Partida.Vertices[clave] = "nada"
+    }
+}
+
+fun inicializarAristas() {
+    val claves = listOf(
+        "27", "38", "49", "5A", "6B", "7C", "26", "48", "6A", "8C", "25", "36", "47", "58", "69", "7A",
+        "8B", "9C", "24", "46", "68", "8A", "AC", "23", "34", "45", "56", "67", "78", "89", "9A", "AB",
+        "BC", "22", "44", "66", "88", "AA", "CC", "32", "43", "54", "65", "76", "87", "98", "A9", "BA",
+        "CB", "42", "64", "86", "A8", "CA", "52", "63", "74", "85", "96", "A7", "B8", "C9", "62", "84",
+        "A6", "C8", "72", "83", "94", "A5", "B6", "C7"
+    )
+    for (clave in claves) {
+        Partida.Aristas[clave] = "nada"
+    }
+}
+
+
